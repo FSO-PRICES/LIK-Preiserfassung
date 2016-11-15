cf271d623849f007fe342c2c82ad680d4b7fb188
Wayne Maurer <wmaurer@users.noreply.github.com>
2016-11-15 09:32:13 +0100

    first version works on lik tablet with crosswalk plugin

---
+++ preiserfasser/preiserfasser-tablet/src/pages/price-entry/price-entry.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'price-entry',
+    templateUrl: 'price-entry.html'
+})
+export class PriceEntryPage {
+    public items = [
+        'foo',
+        'bar',
+        'fee',
+        'fum',
+        'foo',
+        'bar',
+        'fee',
+        'fum',
+        'foo',
+        'bar',
+        'fee',
+        'fum',
+        'foo',
+        'bar',
+        'fee',
+        'fum',
+        'foo',
+        'bar',
+        'fee',
+        'fum',
+        'foo',
+        'bar',
+        'fee',
+        'fum',
+        'foo',
+        'bar',
+        'fee',
+        'fum',
+        'foo',
+        'bar',
+        'fee',
+        'fum',
+        'foo',
+        'bar',
+        'fee',
+        'fum',
+    ];
+    constructor() {
+    }
+}
