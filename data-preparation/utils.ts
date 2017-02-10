8da4727697e093ab9bae1fea611bd030cfd5750f
Philipp Schaerer <philipp.schaerer@lambda-it.ch>
2017-02-10 17:56:45 +0100

    Fixed line splitting while parsing files

--- data-preparation/utils.ts
+++ data-preparation/utils.ts
@@ -3,7 +3,7 @@ import * as _ from 'lodash';
 
 export function bufferToCells(buffer: Buffer) {
     const s: string = encoding.convert(buffer, 'UTF-8', 'Latin_1').toString();
-    const lines = s.split('\u000a');
+    const lines = s.split(/\r?\n/); // s.split('\u000a'); This is incompatible with dos format
     return _.drop(lines.filter(x => x.length)).map(x => x.split(';'));
 }
 
