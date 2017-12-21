467e8b529c65f5491c007ba1a6a667ac28c0d9d3
Wayne Maurer <wmaurer@users.noreply.github.com>
2017-12-21 13:31:01 +0100

    release 2.00.006

--- backoffice/backoffice-browser/src/environments/environment.ts
+++ backoffice/backoffice-browser/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    version: '2.00.005',
+    version: '2.00.006',
 
     defaultSettings: {
         serverConnection: {
--- preiserfasser/preiserfasser-tablet/src/environments/environment.prod.ts
+++ preiserfasser/preiserfasser-tablet/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    version: '2.00.005',
+    version: '2.00.006',
 };
