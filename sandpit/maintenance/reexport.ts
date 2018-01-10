1d05d41b31a72405fa08df18e0a12c055cc68e24
Wayne Maurer <wmaurer@users.noreply.github.com>
2018-01-10 15:19:30 +0100

    reexport code stashed for maintenance

---
+++ sandpit/maintenance/reexport.ts
@@ -0,0 +1,5 @@
+// const xxx = (x.rows as any[]).find(row => row.id === '1515069593977').doc
+// .preismeldungIds;
+// const preismeldungenToExport = preismeldungBags.filter(
+// bag => bag.pm.istAbgebucht && xxx.some(y => y === bag.pm._id)
+// );
