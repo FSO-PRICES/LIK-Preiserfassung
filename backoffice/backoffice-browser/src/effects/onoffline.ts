404e8a977c2324b247c8224d971462e8bfc3fca2
Wayne Maurer <wmaurer@users.noreply.github.com>
2018-01-29 15:01:14 +0100

    fix login problem with online / offline

--- backoffice/backoffice-browser/src/effects/onoffline.ts
+++ backoffice/backoffice-browser/src/effects/onoffline.ts
@@ -3,6 +3,8 @@ import { Effect, Actions } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 
+import { continueEffectOnlyIfTrue } from '../common/effects-extensions';
+
 import { Models as P } from 'lik-shared';
 
 import * as fromRoot from '../reducers';
@@ -13,12 +15,14 @@ import { CurrentSetting } from '../reducers/setting';
 @Injectable()
 export class OnOfflineEffects {
     currentSetting$ = this.store.select(fromRoot.getCurrentSettings);
+    isLoggedIn$ = this.store.select(fromRoot.getIsLoggedIn);
 
     constructor(private actions$: Actions, private store: Store<fromRoot.AppState>) {}
 
     @Effect()
     loadOnOffline$ = this.actions$
         .ofType('LOAD_ONOFFLINE')
+        .let(continueEffectOnlyIfTrue(this.isLoggedIn$))
         .flatMap(() =>
             Observable.from(getOnOfflineStatus()).map(
                 payload => ({ type: 'LOAD_ONOFFLINE_SUCCESS', payload } as onoffline.Action)
@@ -28,6 +32,7 @@ export class OnOfflineEffects {
     @Effect()
     toggleOnOffline$ = this.actions$
         .ofType('TOGGLE_ONOFFLINE')
+        .let(continueEffectOnlyIfTrue(this.isLoggedIn$))
         .flatMap(() =>
             Observable.from(toggleOnOfflineStatus()).map(
                 payload => ({ type: 'LOAD_ONOFFLINE_SUCCESS', payload } as onoffline.Action)
