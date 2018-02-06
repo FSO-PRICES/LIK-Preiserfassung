e46418020d6aba3e61d8c8d97675057d37ba6c08
Philipp Schaerer <philipp.schaerer@lambda-it.ch>
2018-02-06 13:23:07 +0100

    Cleanup unecessary code

--- backoffice/backoffice-browser/src/components/pef-zoom/pef-zoomable.directive.ts
+++ backoffice/backoffice-browser/src/components/pef-zoom/pef-zoomable.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Input, Output, EventEmitter, OnChanges } from '@angular/core';
+import { Directive, ElementRef, Input, OnChanges } from '@angular/core';
 
 @Directive({
     selector: '[pefZoomable]',
