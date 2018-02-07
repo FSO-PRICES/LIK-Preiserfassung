267363ce92647431244273db066c6547f39a65b1
Philipp Schaerer <philipp.schaerer@lambda-it.ch>
2018-02-07 16:59:40 +0100

    Added fullscreen mode.

--- backoffice/backoffice-browser/src/actions/setting.ts
+++ backoffice/backoffice-browser/src/actions/setting.ts
@@ -7,4 +7,5 @@ export type Action =
     | { type: 'SETTING_LOAD'; payload: null }
     | { type: 'SAVE_SETTING_SUCCESS'; payload: P.Setting }
     | { type: 'SAVE_SETTING'; payload: null }
-    | { type: 'UPDATE_SETTING'; payload: P.Setting };
+    | { type: 'UPDATE_SETTING'; payload: P.Setting }
+    | { type: 'TOGGLE_FULLSCREEN'; payload: null };
