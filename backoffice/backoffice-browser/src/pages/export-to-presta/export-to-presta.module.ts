630337a6bd21f3f3496439a438f112212be7096d
Philipp Schaerer <philipp.schaerer@lambda-it.ch>
2018-03-21 17:21:17 +0100

    Removed unecessary code

--- backoffice/backoffice-browser/src/pages/export-to-presta/export-to-presta.module.ts
+++ backoffice/backoffice-browser/src/pages/export-to-presta/export-to-presta.module.ts
@@ -3,21 +3,11 @@ import { IonicPageModule } from 'ionic-angular';
 import { CommonModule } from '@angular/common';
 
 import { ExportToPrestaPage } from './export-to-presta';
-import { PreismeldungenExportComponent } from './preismeldungen/preismeldungen-export';
-import { PreiserheberExportComponent } from './preiserheber/preiserheber-export';
 
 import { PefMenuModule } from '../../components/pef-menu/pef-menu.module';
 
 @NgModule({
-    declarations: [
-        ExportToPrestaPage,
-        PreismeldungenExportComponent,
-        PreiserheberExportComponent,
-    ],
-    imports: [
-        IonicPageModule.forChild(ExportToPrestaPage),
-        PefMenuModule
-    ],
+    declarations: [ExportToPrestaPage],
+    imports: [IonicPageModule.forChild(ExportToPrestaPage), PefMenuModule],
 })
-export class ExportToPrestaModule {
-}
+export class ExportToPrestaModule {}
