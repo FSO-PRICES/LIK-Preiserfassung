ee3e42a582693eb2a9b5f649fc87a8c76bb501ff
Philipp Schaerer <philipp.schaerer@lambda-it.ch>
2018-04-06 16:22:39 +0200

    Fixed ionic refresh problem. refs #197

--- preiserfasser/preiserfasser-tablet/src/pages/dialog-new-pm-bearbeitungs-code/dialog-new-pm-bearbeitungs-code.ts
+++ preiserfasser/preiserfasser-tablet/src/pages/dialog-new-pm-bearbeitungs-code/dialog-new-pm-bearbeitungs-code.ts
@@ -1,15 +1,14 @@
-import { Component, EventEmitter } from '@angular/core';
+import { Component, EventEmitter, ChangeDetectorRef } from '@angular/core';
 import { ViewController, IonicPage } from 'ionic-angular';
 
 @IonicPage()
 @Component({
     selector: 'dialog-new-pm-bearbeitungs-code',
     templateUrl: 'dialog-new-pm-bearbeitungs-code.html',
-    host: { '[class.pef-dialog]': 'true' }
+    host: { '[class.pef-dialog]': 'true' },
 })
 export class DialogNewPmBearbeitungsCodeComponent {
-    constructor(public viewCtrl: ViewController) {
-    }
+    constructor(public viewCtrl: ViewController, private changeDetection: ChangeDetectorRef) {}
 
     private _bearbeitungscode = 3;
     get bearbeitungscode() {
@@ -17,5 +16,7 @@ export class DialogNewPmBearbeitungsCodeComponent {
     }
     set bearbeitungscode(v) {
         this._bearbeitungscode = +v;
+        // Seems to be a ionic bug, trigger change detection manually
+        setTimeout(() => this.changeDetection.detectChanges(), 0);
     }
 }
