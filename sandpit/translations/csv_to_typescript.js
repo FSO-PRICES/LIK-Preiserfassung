1b90a59838e85255ae593eac32ead716a5fa3814
Philipp Schaerer <philipp.schaerer@lambda-it.ch>
2018-06-18 14:07:05 +0200

    Unlocked englisch translation for apps

--- sandpit/translations/csv_to_typescript.js
+++ sandpit/translations/csv_to_typescript.js
@@ -2,14 +2,19 @@ const fs = require('fs');
 const Papa = require('papaparse');
 
 const data = fs.readFileSync('./translations.csv', 'utf8');
-const result = Papa.parse(data.substr(0, data.length - 1), { header: false })
+const result = Papa.parse(data.substr(0, data.length - 1), { header: false });
 
-const typescript = `${createTypescript('de', '1')}\n\n${createTypescriptKeys('en')}\n\n${createTypescript('fr', '3')}\n\n${createTypescript('it', '4')}\n`;
+const typescript = `${createTypescript('de', '1')}\n\n${createTypescript('en', '2')}\n\n${createTypescript(
+    'fr',
+    '3'
+)}\n\n${createTypescript('it', '4')}\n`;
 // const typescript = `${createTypescript('it', '4')}\n`;
 fs.writeFileSync('translations.ts', typescript, 'utf8');
 
 function createTypescript(code, index) {
-    return `export const ${code} = {\n${result.data.map(v => `    '${v['0']}': '${v[index].replace(/'/g, `\\'`)}'`).join(',\n')}\n};`;
+    return `export const ${code} = {\n${result.data
+        .map(v => `    '${v['0']}': '${v[index].replace(/'/g, `\\'`)}'`)
+        .join(',\n')}\n};`;
 }
 
 function createTypescriptKeys(code) {
