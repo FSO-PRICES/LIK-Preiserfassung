87abcb6253a87594c8e1952ac65f69cd3b5b9f4d
Philipp Schaerer <philipp.schaerer@lambda-it.ch>
2018-08-03 10:56:56 +0200

    Fixed session cookie persistance problem with electron. refs #382

--- backoffice/backoffice-electron-win/main.js
+++ backoffice/backoffice-electron-win/main.js
@@ -15,8 +15,8 @@ const template = [
             { role: 'paste' },
             { role: 'pasteandmatchstyle' },
             { role: 'delete' },
-            { role: 'selectall' }
-        ]
+            { role: 'selectall' },
+        ],
     },
     {
         label: 'View',
@@ -26,16 +26,13 @@ const template = [
             { role: 'toggledevtools' },
             { type: 'separator' },
             { type: 'separator' },
-            { role: 'togglefullscreen' }
-        ]
+            { role: 'togglefullscreen' },
+        ],
     },
     {
         role: 'window',
-        submenu: [
-            { role: 'minimize' },
-            { role: 'close' }
-        ]
-    }
+        submenu: [{ role: 'minimize' }, { role: 'close' }],
+    },
 ];
 
 // Module to create native browser window.
@@ -54,17 +51,19 @@ function createWindow() {
     mainWindow.maximize();
 
     // and load the index.html of the app.
-    mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'ionic-browser-app', 'index.html'),
-        protocol: 'file:',
-        slashes: true
-    }));
+    mainWindow.loadURL(
+        url.format({
+            pathname: path.join(__dirname, 'ionic-browser-app', 'index.html'),
+            protocol: 'file:',
+            slashes: true,
+        })
+    );
 
     // Open the DevTools.
     // mainWindow.webContents.openDevTools();
 
     // Emitted when the window is closed.
-    mainWindow.on('closed', function () {
+    mainWindow.on('closed', function() {
         // Dereference the window object, usually you would store windows
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
@@ -72,17 +71,47 @@ function createWindow() {
     });
 }
 
+function setCookiesPersistent() {
+    const cookies = electron.session.defaultSession.cookies;
+    cookies.on('changed', function(event, cookie, cause, removed) {
+        if ((cookie.session || cookie.expirationDate === undefined) && cookie.httpOnly && !removed) {
+            const newCookie = {
+                url: `http://${cookie.domain}`,
+                domain: cookie.domain,
+                name: cookie.name,
+                value: cookie.value,
+                domain: cookie.domain,
+                path: cookie.path,
+                secure: false,
+                hostOnly: false,
+                httpOnly: cookie.httpOnly,
+                session: false,
+                // Make cookie persistent and set expiration date to 1 year, if set otherwise on the server, the login window will pop up
+                expirationDate: Math.floor(new Date().getTime() / 1000) + 31536000,
+            };
+            cookies.set(newCookie, function(err, x) {
+                if (err) {
+                    console.log('Error trying to persist cookie', err, cookie);
+                }
+            });
+            cookies.flushStore(() => {});
+        }
+    });
+}
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', function () {
+app.on('ready', function() {
     createWindow();
     const menu = Menu.buildFromTemplate(template);
     Menu.setApplicationMenu(menu);
+    // The session cookie doesn't persist, transforming it into a cookie with expiration date marks it persistent
+    setCookiesPersistent();
 });
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on('window-all-closed', function() {
     // On OS X it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
@@ -90,7 +119,7 @@ app.on('window-all-closed', function () {
     }
 });
 
-app.on('activate', function () {
+app.on('activate', function() {
     // On OS X it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (mainWindow === null) {
