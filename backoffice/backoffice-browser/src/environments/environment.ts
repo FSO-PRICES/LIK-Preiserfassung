40719c65e3c0ea7971df1caea69c7e658e961588
Philipp Schaerer <philipp.schaerer@lambda-it.ch>
2018-08-06 12:58:28 +0200

    Updated verison to 2.1.011

--- backoffice/backoffice-browser/src/environments/environment.ts
+++ backoffice/backoffice-browser/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    version: '2.1.010',
+    version: '2.1.011',
 
     defaultSettings: {
         serverConnection: {
--- preiserfasser/preiserfasser-tablet/src/environments/environment.prod.ts
+++ preiserfasser/preiserfasser-tablet/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    version: '2.1.010',
+    version: '2.1.011',
 };
