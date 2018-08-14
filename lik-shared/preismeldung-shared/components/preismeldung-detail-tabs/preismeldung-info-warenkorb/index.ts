73b34fa2231523c9a099af6b9891182126577340
Philipp Schaerer <philipp.schaerer@lambda-it.ch>
2018-08-14 16:49:49 +0200

    Added preamble license text. GPL-3

--- lik-shared/preismeldung-shared/components/preismeldung-detail-tabs/preismeldung-info-warenkorb/index.ts
+++ lik-shared/preismeldung-shared/components/preismeldung-detail-tabs/preismeldung-info-warenkorb/index.ts
@@ -1,1 +1,21 @@
+/*
+ * LIK-Preiserfassung
+ * Copyright (C) 2018 Bundesbehörden der Schweizerischen Eidgenossenschaft - Bundesamt für Statistik
+ *
+ * This file is part of LIK-Preiserfassung.
+ *
+ * LIK-Preiserfassung is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * LIK-Preiserfassung is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LIK-Preiserfassung. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 export * from './preismeldung-info-warenkorb';
