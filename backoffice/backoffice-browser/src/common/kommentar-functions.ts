e716592c38c82147c87f2e7934d8f3a17327df2d
Philipp Schaerer <philipp.schaerer@lambda-it.ch>
2018-10-16 13:59:15 +0200

    Fixed multi translation of comments. refs #400

--- backoffice/backoffice-browser/src/common/kommentar-functions.ts
+++ backoffice/backoffice-browser/src/common/kommentar-functions.ts
@@ -2,9 +2,10 @@ import * as translations from 'lik-shared/translations';
 
 export function translateKommentare(kommentar: string) {
     const kommentare = kommentar.split('¶');
-    const translatableKommentare = kommentare.slice(0, -1);
+    const rawKommentare = kommentare.slice(0, -1)[0];
+    const translatableKommentare = !rawKommentare ? [] : rawKommentare.split(',');
     return (
-        translatableKommentare.map(x => (translations.de[x] ? `AT: ${translations.de[x]}` : x)).join('¶') +
+        translatableKommentare.map(x => (translations.de[x] ? `AT: ${translations.de[x]}` : x)).join(', ') +
         (translatableKommentare.length > 0 ? '¶' : '') +
         kommentare.slice(-1)
     );
