ded6bd2aeb09895321b931dbf73bba1a92d6829a
Philipp Schaerer <philipp.schaerer@lambda-it.ch>
2018-12-17 13:05:29 +0100

    Updated to version 2.1.017

--- backoffice/backoffice-browser/src/environments/environment.ts
+++ backoffice/backoffice-browser/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    version: '2.1.016',
+    version: '2.1.017',
 
     masterErhebungsorgannummer: '69',
 
--- preiserfasser/preiserfasser-tablet/src/environments/environment.ts
+++ preiserfasser/preiserfasser-tablet/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    version: '2.1.016',
+    version: '2.1.017',
 };
