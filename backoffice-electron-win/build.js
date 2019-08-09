0cc749eb5de6226c202208396de1c9ba5154a472
Philipp Schaerer <philipp.schaerer@lambda-it.ch>
2019-08-09 11:39:29 +0200

    Improved and added build steps in package.json

--- backoffice-electron-win/build.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-var electronInstaller = require('electron-winstaller');
-resultPromise = electronInstaller.createWindowsInstaller({
-    appDirectory: 'C:\temp\build\bfs-lik-preiserfasser-win-win32-x64',
-    outputDirectory: 'c:\temp\build\bfs-lik-preiserfasser-win-installer',
-    authors: 'Lambda IT GmbH',
-    exe: 'bfs-lik-preiserfasser-install.exe'
-});
-resultPromise.then(() => console.log("It worked!"), (e) => console.log(`No dice: ${e.message}`));
--- backoffice-electron-win/package-app.bat
+++
@@ -1,3 +0,0 @@
-robocopy ..\backoffice-browser\platforms\browser\www .\ionic-browser-app /mir
-rmdir /S /Q c:\temp\build\bfs-lik-backoffice-win-win32-ia32
-node_modules\.bin\electron-packager . --tmpdir=c:\temp --out=c:\temp\build --platform=win32 --arch=ia32 --no-prune --appname=bfs-lik-preiserfasser.exe --app-copyright="Schweizerische Eidgenossenschaft Bundesamt fuer Statistik" --win32metadata.CompanyName="Schweizerische Eidgenossenschaft Bundesamt fuer Statistik" --win32metadata.ProductName="BFS LIK Backoffice" --win32metadata.FileDescription="BFS LIK Backoffice"
