004e50dd20c40f29e9fd64dd1bc89ea456db28e9
Philipp Schaerer <philipp.schaerer@lambda-it.ch>
2019-08-12 10:46:36 +0200

    Updated to version 2.2.002-alpha.2

--- apps/backoffice/src/environments/environment.ts
+++ apps/backoffice/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    version: '2.2.002-alpha.1',
+    version: '2.2.002-alpha.2',
 
     masterErhebungsorgannummer: '69',
 
--- apps/preiserfasser/src/environments/environment.prod.ts
+++ apps/preiserfasser/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    version: '2.2.002-alpha.1',
+    version: '2.2.002-alpha.2',
 };
