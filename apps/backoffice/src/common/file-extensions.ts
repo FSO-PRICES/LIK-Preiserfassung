39ed18e90d7fc9a3a7c6c474de1d9532f18bc25a
Philipp Schaerer <philipp.schaerer@lambda-it.ch>
2020-02-04 10:44:55 +0100

    Added newline type recocnition on import

--- apps/backoffice/src/common/file-extensions.ts
+++ apps/backoffice/src/common/file-extensions.ts
@@ -1,26 +1,55 @@
 import * as Papa from 'papaparse';
-import { Observable, Observer } from 'rxjs';
+import { from, Observable, Observer } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
-const defaultParseSettings = { delimiter: ';', quoteChar: '"', skipEmptyLines: true, encoding: 'ISO-8859-1' };
+const defaultParseSettings = (newline: string) => ({
+    delimiter: ';',
+    quoteChar: '"',
+    skipEmptyLines: true,
+    encoding: 'ISO-8859-1',
+    newline,
+});
 
 export function parseCsvText(text: string) {
-    return Papa.parse(text, defaultParseSettings).data || [];
+    return Papa.parse(text, defaultParseSettings(getNewlineStyle(text))).data || [];
 }
 
-export function parseCsvAsObservable(file: any): Observable<any> {
-    return Observable.create((observer: Observer<any>) => {
-        Papa.parse(file, {
-            ...defaultParseSettings,
-            complete: results => {
-                results.data.shift();
-                observer.next(results.data);
-                observer.complete();
-            },
-        });
-    });
+export function parseCsvAsObservable(file: File): Observable<any> {
+    return from(getFileContents(file)).pipe(
+        switchMap(text =>
+            Observable.create((observer: Observer<any>) => {
+                Papa.parse(file, {
+                    ...defaultParseSettings(getNewlineStyle(text)),
+                    complete: results => {
+                        results.data.shift();
+                        observer.next(results.data);
+                        observer.complete();
+                    },
+                });
+            }),
+        ),
+    );
 }
 
 export function toCsv(data: any[], header: boolean = true, quote: boolean = false): string {
     // @types/papaparse is not available for v4.3.3
     return Papa.unparse(data, { delimiter: ';', header, quoteChar: '', quote });
+}
+
+function getNewlineStyle(text: string) {
+    const crlfIndex = text.indexOf('\r\n');
+    return crlfIndex > 0 ? '\r\n' : '\n';
+}
+
+function getFileContents(file: File): Promise<string> {
+    const reader = new FileReader();
+    return new Promise((resolve, reject) => {
+        reader.readAsText(file, 'ISO-8859-1');
+        reader.onload = (evt: any) => {
+            resolve(evt.target.result);
+        };
+        reader.onerror = () => {
+            reject('error reading file');
+        };
+    });
 }
