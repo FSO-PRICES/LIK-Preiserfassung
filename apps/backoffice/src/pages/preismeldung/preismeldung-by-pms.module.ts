4bacea21d38550c43fe1e2caad5bebe3058a9a76
Philipp Schaerer <philipp.schaerer@lambda-it.ch>
2020-02-19 09:30:21 +0100

    Fixed preismeldung template errors

--- apps/backoffice/src/pages/preismeldung/preismeldung-by-pms.module.ts
+++ apps/backoffice/src/pages/preismeldung/preismeldung-by-pms.module.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 
 import { PreismeldungByPmsPage } from './preismeldung-by-pms';
 import { PreismeldungPagesModule } from './preismeldung-pages.module';
 
 @NgModule({
+    imports: [CommonModule, IonicModule, PreismeldungPagesModule],
     declarations: [PreismeldungByPmsPage],
-    imports: [PreismeldungPagesModule],
 })
 export class PreismeldungByPmsModule {}
--- apps/backoffice/src/pages/preismeldung/preismeldung.module.ts
+++ apps/backoffice/src/pages/preismeldung/preismeldung.module.ts
@@ -2,11 +2,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
+import { PreismeldungSharedModule } from '@lik-shared';
+
 import { PreismeldungPage } from './preismeldung';
 import { PreismeldungPagesModule } from './preismeldung-pages.module';
 
 @NgModule({
-    imports: [CommonModule, IonicModule, PreismeldungPagesModule],
+    imports: [CommonModule, IonicModule, PreismeldungPagesModule, PreismeldungSharedModule],
     declarations: [PreismeldungPage],
 })
 export class PreismeldungModule {}
