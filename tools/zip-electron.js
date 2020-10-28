892d26b922d2bb70d69c3fd1f5fa9aaa42862a2c
Philipp Schaerer <philipp.schaerer@lambda-it.ch>
2020-10-28 09:58:23 +0100

    Fixed zip script

--- tools/zip-electron.js
+++ tools/zip-electron.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 
 const config = JSON.parse(fs.readFileSync('../package.json'));
 const version = `v${config.version}`;
-const basePath = (app, platform) => `apps/${app}/platforms/electron/build/${platform}/`;
+const basePath = (app, platform) => `../apps/${app}/platforms/electron/build/${platform}/`;
 const projects = [
     {
         name: 'zip-electron-pe-linux',
@@ -35,7 +35,7 @@ projects.forEach(proj => {
         gulp
             .src(`${proj.path}**/*`, { base: proj.path, dot: true })
             .pipe(zip(proj.file))
-            .pipe(gulp.dest(`./dist/`)),
+            .pipe(gulp.dest(`../dist/`)),
     );
 });
 gulp.task('update-tags', done => {
